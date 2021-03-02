c2ee22176ffe2759ac9191950de2cc1d6fc49ca8
Andy Weiss <andyweiss1982@gmail.com>
2021-03-02 10:49:26 -0500

    extract form component and add default image

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Game from "./components/Game";
+import Form from "./components/Form";
 // import Search from "./components/Search";
 // import Button from "./components/Button";
 
@@ -32,17 +33,11 @@ const App = () => {
       <header>
         <h1>🎮 I'd Rather Be Playing 🕹</h1>
       </header>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="search">Search</label>
-        <input
-          id="search"
-          name="search"
-          autoComplete="off"
-          value={searchTerm}
-          onChange={(event) => setSearchTerm(event.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <Form
+        handleSubmit={handleSubmit}
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+      />
       <ul id="games">
         {games.map((game) => (
           <Game key={game.id} name={game.name} image={game.background_image} />
